2619be9d5ce004d8f38fbf9de102c30553aea65e
YuliaSimanavets <mrs.simonovets@gmail.com>
2024-05-13 17:34:27 +0200

    created project

---
+++ AuthorizationApp/AuthorizationApp/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  AuthorizationApp
+//
+//  Created by Yuliya on 13/05/2024.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+    }
+}
